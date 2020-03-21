8e081ea00cea19df59b6a346cc25bdb481822c5c
lazhero <54720338+lazhero@users.noreply.github.com>
2020-03-21 10:01:10 -0600

    [DAT-49] First Version LayoutGenerator

---
+++ src/cr/ac/tec/Board/LayoutCreation.java
@@ -0,0 +1,39 @@
+package cr.ac.tec.Board;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+
+public class LayoutCreation {
+    /*
+    A class to create some JAvafx containers
+     */
+
+    /**
+     * Creates an anchorpane
+     * @param width sets the anchor panel width's
+     * @param heigth sets the anchor height's
+     * @return the anchor panel
+     */
+    public static AnchorPane Anchor(double width,double heigth){
+        AnchorPane AnchorP =new AnchorPane();
+        AnchorP.setPrefHeight(heigth);
+        AnchorP.setPrefWidth(width);
+        return AnchorP;
+
+    }
+
+
+    /**
+     *
+     * @param width the scroll width's
+     * @param heigth the Scrolls height's
+     * @return the scrollpane
+     */
+    public static ScrollPane scroll(double width,double heigth){
+        ScrollPane scrolls=new ScrollPane();
+        scrolls.setPrefViewportHeight(heigth);
+        scrolls.setPrefViewportWidth(width);
+        return scrolls;
+    }
+
+}
---
+++ src/cr/ac/tec/Board/LayoutNewContent.java
@@ -0,0 +1,39 @@
+package cr.ac.tec.Board;
+
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+
+public class LayoutNewContent {
+    /*
+    This class add or changes a container's content
+     */
+
+    /**
+     * Add a new Node to an anchorPane
+     * @param anchorPane the AnchorPane
+     * @param node The widget to be added
+     * @param Top the distance from the Top,if  is 0 wont be setted
+     * @param Bottom the distance from the Bottom,if  is 0 wont be setted
+     * @param Right the distance from the Right,if  is 0 wont be setted
+     * @param Left the distance from the Left,if  is 0 wont be setted
+     */
+    public static void Add(AnchorPane anchorPane,Node node,double Top,double Bottom,double Right,double Left){
+        anchorPane.getChildren().add(node);
+        if(Top!=0.0) anchorPane.setTopAnchor(node,Top);
+        if(Bottom!=0.0)anchorPane.setBottomAnchor(node,Bottom);
+        if(Left!=0.0)anchorPane.setLeftAnchor(node,Left);
+        if(Right!=0.0)anchorPane.setRightAnchor(node,Right);
+    }
+
+    /**
+     * Uddates a scrolls content's
+     * @param scroll The Scroll
+     * @param node The new content
+     */
+    public static void Update(ScrollPane scroll,Node node){
+
+        scroll.setContent(node);
+    }
+
+}
