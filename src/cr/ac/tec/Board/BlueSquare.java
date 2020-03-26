fe10e9a27acc4ebf357dc141e3304139cc9a583e
lazhero <54720338+lazhero@users.noreply.github.com>
2020-03-26 17:31:02 -0600

    [DAT-11] First BlueSquare's version

---
+++ src/cr/ac/tec/Board/BlueSquare.java
@@ -0,0 +1,15 @@
+package cr.ac.tec.Board;
+
+import javafx.scene.paint.Paint;
+
+public class BlueSquare extends Square {
+    BlueSquare(Paint color, double coordx, double coordy, double sideSize){
+        super(color,coordx,coordy,sideSize);
+
+    }
+
+    @Override
+    public void DuelTime() {
+        //lacks event
+    }
+}
