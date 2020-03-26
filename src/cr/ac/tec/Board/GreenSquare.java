43c29031a56da05e60b1ae0101bb4678883b844d
lazhero <54720338+lazhero@users.noreply.github.com>
2020-03-26 17:21:10 -0600

    [DAT-12] First GreenSquare version

---
+++ src/cr/ac/tec/Board/GreenSquare.java
@@ -0,0 +1,15 @@
+package cr.ac.tec.Board;
+
+import javafx.scene.paint.Paint;
+
+public class GreenSquare extends Square {
+    GreenSquare(Paint color, double coordx, double coordy, double sideSize){
+        super(color,coordx,coordy,sideSize);
+
+    }
+
+    @Override
+    public void DuelTime() {
+        //lacks event
+    }
+}
