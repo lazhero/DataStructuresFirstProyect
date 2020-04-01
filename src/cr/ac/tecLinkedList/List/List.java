15e4b3ed204ff64ea2c39e3a06752da679fadec3
lazhero <54720338+lazhero@users.noreply.github.com>
2020-04-01 17:42:09 -0600

    [DAT-68] An interface added to model the defaul methods to each list

---
+++ src/cr/ac/tecLinkedList/List/List.java
@@ -0,0 +1,17 @@
+package cr.ac.tecLinkedList.List;
+
+import cr.ac.tecLinkedList.Nodes.DoubleNode;
+
+public interface List<T> {
+    public abstract void AddHead(T NewInfo);
+    public abstract void AddTail(T Newinfo);
+    public abstract T DeleteFromHead();
+    public abstract T DeleteFromTail();
+    public abstract boolean isEmpty();
+    public abstract boolean inList(T info);
+
+    public abstract T get(int position);
+    public abstract void delete(int position);
+
+    public abstract int getLength();
+}
