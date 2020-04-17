b723f033436917f6aebb4921d5593d90dff707be
lazhero <54720338+lazhero@users.noreply.github.com>
2020-04-17 11:53:23 -0600

    [DAT-78] Adding the hand's duel

---
+++ src/cr/ac/tec/Minigames/PRS/HandsDuel.java
@@ -0,0 +1,15 @@
+package cr.ac.tec.Minigames.PRS;
+
+public class HandsDuel {
+    public static int winner(Hand hand1,Hand hand2){
+        int FirstHandValue=hand1.getValue();
+        int SecondHandValue= hand2.getValue();
+        if((FirstHandValue+1)%3==SecondHandValue){
+            return 1;
+        }
+        else if(FirstHandValue==SecondHandValue){
+            return 0;
+        }
+        else return 2;
+    }
+}
