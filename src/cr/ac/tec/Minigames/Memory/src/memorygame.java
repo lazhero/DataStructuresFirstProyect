9fe50564baf398c2f814e547db56cb878e1cdc21
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-04-18 11:16:39 -0600

    [DAT-87] troubleshoot with events

--- src/cr/ac/tec/Minigames/Memory/src/memorygame.java
+++ src/cr/ac/tec/Minigames/Memory/src/memorygame.java
@@ -3,7 +3,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -13,16 +12,19 @@ import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
-
 import javafx.scene.input.MouseEvent;
 
 
-
+import java.security.cert.TrustAnchor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javafx.util.Duration;
-
+import java.util.EventObject;
 
 
 public class memorygame extends Application {
+
     private static final int NUM_OF_PAIRS=8;
     private  static final int NUM_PER_ROW=4;
     private Tile selected = null;
@@ -35,8 +37,8 @@ public class memorygame extends Application {
     private Text turn= new Text();
     private Text score1=new Text("Score:"+ scorep1);
     private Text score2=new Text("Score:"+ scorep2);
-    public Parent createContent(){
 
+    public Parent createContent(){
 
 
         Pane root = new Pane();
@@ -61,6 +63,24 @@ public class memorygame extends Application {
 
 
 
+        char c='A';
+        List<Tile> tiles =new ArrayList<>();
+        for(int i=0; i<NUM_OF_PAIRS;i++){
+            tiles.add(new Tile(String.valueOf(c)));
+            tiles.add(new Tile(String.valueOf(c)));
+            c++;
+
+
+
+        }
+        Collections.shuffle(tiles);
+        for (int i=0;i<tiles.size();i++){
+            Tile tile = tiles.get(i);
+            tile.setTranslateX(130*((i)%NUM_PER_ROW)+80);
+            tile.setTranslateY(130*((i)/NUM_PER_ROW)+10);
+            root.getChildren().add(tile);
+
+        }
 
         Text player1t = new Text("Player 1");
         player1t.setTranslateY(580);
@@ -82,12 +102,20 @@ public class memorygame extends Application {
         score2.setFont(Font.font(27));
 
 
+
+
+
         root.getChildren().addAll(player1t,player2t,score1,score2);
 
         return root;
 
 
     }
+
+
+
+
+
     public class Tile extends StackPane{
 
         private Text text= new Text();
@@ -104,7 +132,9 @@ public class memorygame extends Application {
 
             getChildren().addAll(border,text);
 
+            setOnMouseClicked(this::handleMouseClick);
 
+            close();
         }
         public void handleMouseClick(MouseEvent event){
             if (isOpen() || clickCount==0)
@@ -122,13 +152,17 @@ public class memorygame extends Application {
                         this.close();
                         cont=cont+1;
                         if(cont%2==0){
+                            changeturn("Player 1 turn");
+                        }
+
+                        else changeturn("Player 2 turn");
 
                     }
                     else{
                         if (cont%2==0){
                             correct+=1;
                             scorep1=scorep1+10;
-
+                            changeturn("Player 2 turn");
                             score1.setText("Score:"+ scorep1);
                             System.out.println("player 1  "+scorep1);
                             cont=cont+1;
@@ -138,7 +172,7 @@ public class memorygame extends Application {
                             scorep2=scorep2+10;
                             score2.setText("Score:"+ scorep2);
                             System.out.println("EStoy aqui");
-
+                            changeturn("Player 1 turn");
                             System.out.println("player 2  "+ scorep2);;
                             cont=cont+1;
                             System.out.println(correct);
@@ -151,10 +185,10 @@ public class memorygame extends Application {
                         if (correct==8){
 
                             if(scorep1>scorep2){
-
+                                winner(1);
                             }
                             else{
-
+                                winner(2);
                             }
 
                         }
@@ -167,50 +201,44 @@ public class memorygame extends Application {
 
 
         }
+        public  boolean isOpen(){
+            return text.getOpacity()==1;
+
+        }
 
 
 
         }
 
-    }
-    public  boolean isOpen(){
-        return text.getOpacity()==1;
+        public void open(Runnable action){
+            FadeTransition ft = new FadeTransition(Duration.seconds(0.5),text);
+            ft.setToValue(1);
+            ft.setOnFinished(e-> action.run());
+
+
+            ft.play();
+
+        }
+
+        public void close(){
+            FadeTransition ft = new FadeTransition(Duration.seconds(1.5),text);
+            ft.setToValue(0);
+            ft.play();
+        }
+        public boolean hasSameValue(Tile other){
+            return text.getText().equals(other.text.getText());
+        }
 
     }
-    public void open(Runnable action){
-        FadeTransition ft = new FadeTransition(Duration.seconds(0.5),text);
-        ft.setToValue(1);
-        ft.setOnFinished(e-> action.run());
 
-
-        ft.play();
-
-    }
-
-    public void close(){
-        FadeTransition ft = new FadeTransition(Duration.seconds(1.5),text);
-        ft.setToValue(0);
-        ft.play();
-    }
-    public boolean hasSameValue(Tile other){
-        return text.getText().equals(other.text.getText());
-    }
-
-}
-
-
-
-    public static void main (String[]args){
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception{
         primaryStage.setScene(new Scene(createContent()));
         primaryStage.setTitle("Memory Game, GOOD LUCK!");
         primaryStage.show();
-
-
-
     }
 }
