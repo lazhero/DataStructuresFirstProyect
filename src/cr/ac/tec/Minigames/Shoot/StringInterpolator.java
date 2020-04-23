b8ffa27f67ec5aabc539b09d2d31c226d9d58375
lazhero <54720338+lazhero@users.noreply.github.com>
2020-04-23 17:00:37 -0600

    [DAT-105] The string interpolator with a control timer done

---
+++ src/cr/ac/tec/Minigames/Shoot/StringInterpolator.java
@@ -0,0 +1,60 @@
+package cr.ac.tec.Minigames.Shoot;
+
+import cr.ac.tec.Controls.IntHolder;
+import cr.ac.tec.Random.Random;
+import javafx.animation.Interpolator;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StringInterpolator extends Interpolator {
+    ArrayList<String> List=new ArrayList<>();
+    int RigthPos;
+    IntHolder turns;
+    boolean flag=true;
+    String Currently;
+    Timer myTimer;
+    StringInterpolator(ArrayList<String> List,int Rigthone,IntHolder turns,int pause){
+        this.List=List;
+        this.RigthPos=Rigthone;
+        this.turns=turns;
+        this.myTimer=new Timer();
+        this.Currently=List.get(0);
+        TimerTask task= new TimerTask() {
+            @Override
+            public void run() {
+                flag=true;
+            }
+        };
+        Runnable runnable=()->{
+            myTimer.schedule(task,pause,pause);
+        };
+        Thread t=new Thread(runnable);
+        t.start();
+    }
+    @Override
+    protected double curve(double v) {
+        return 0;
+    }
+
+    @Override
+    public Object interpolate(Object o, Object o1, double v) {
+        if(flag && turns.getSaved()>=0) {
+            if (turns.getSaved() == 0) {
+                Currently= List.get(RigthPos);
+            } else {
+                flag=false;
+                turns.lowSaved(1);
+                int i = Random.RandomNumber(List.size()) - 1;
+                while (i == RigthPos) {
+                    i = Random.RandomNumber(List.size()) - 1;
+                }
+                Currently=List.get(i);
+
+            }
+
+        }
+        return Currently;
+    }
+}
