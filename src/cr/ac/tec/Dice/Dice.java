1f5418ed8062daafd9899a63e6f50725494bc5de
miguemesen <miguemesen12@gmail.com>
2020-04-22 18:41:52 -0600

    [DAT-96] Created dice class.

--- src/cr/ac/tec/Dice/Dice.java
+++ src/cr/ac/tec/Dice/Dice.java
@@ -1,10 +1,14 @@
 package cr.ac.tec.Dice;
 
 public class Dice {
+    private static double diceNumber;
 
     public static void main(String[] args){
     }
 
-
+    private static void randomNumber(){
+        diceNumber= (int)(Math.random()*((6-1)+1))+1;
+        System.out.println(diceNumber);
+    }
 
 }
