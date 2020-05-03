b4dc23db6d6d80a47acf29e951f13e39bd8cdabc
miguemesen <miguemesen12@gmail.com>
2020-05-02 23:38:54 -0600

    [DAT-157] Create an AnimatedPlayer class.

--- src/cr/ac/tec/Minigames/DiamondHunter/Classes/AnimatedPlayer.java
+++ src/cr/ac/tec/Minigames/DiamondHunter/Classes/AnimatedPlayer.java
@@ -1,10 +1,14 @@
 package cr.ac.tec.Minigames.DiamondHunter.Classes;
 
+import cr.ac.tec.Dice.Animation;
 import javafx.scene.canvas.GraphicsContext;
+
+import java.util.HashMap;
 
 public class AnimatedPlayer extends GameObject{
     private int life;
     private String currentAnimation;
+    private HashMap<String, Animation> animations;
 
     public AnimatedPlayer(int x, int y, int velocity, String imageName, int life, String currentAnimation) {
         super(x, y, velocity, imageName);
