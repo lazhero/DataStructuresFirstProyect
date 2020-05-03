3ae6c56b2362b9daaf2872749d5f8e96f81e3f3e
miguemesen <miguemesen12@gmail.com>
2020-05-02 23:42:20 -0600

    [DAT-158] Created an Animation class.

---
+++ src/cr/ac/tec/Minigames/DiamondHunter/Classes/Animation.java
@@ -0,0 +1,35 @@
+package cr.ac.tec.Minigames.DiamondHunter.Classes;
+
+import javafx.scene.shape.Rectangle;
+
+public class Animation {
+    private double duration;
+    private Rectangle coordinates[];
+
+    public Animation(double duration, Rectangle coordinates[]) {
+        this.duration = duration;
+        this.coordinates = coordinates;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Rectangle[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Rectangle[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Rectangle calculateCurrentFrame(double t){
+        int amountOfFrames = this.coordinates.length;
+        int currentFrameIndex = (int)(t % (amountOfFrames * duration)/duration); //Analizar
+        return coordinates[currentFrameIndex];
+    }
+}
