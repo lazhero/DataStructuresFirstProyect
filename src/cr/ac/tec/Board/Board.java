0097cbd5bfbdcfd609b2b5832f9462f042e6a60d
lazhero <54720338+lazhero@users.noreply.github.com>
2020-05-03 11:22:13 -0600

    [DAT-129] Class to prove the game

---
+++ src/cr/ac/tec/Board/Board.java
@@ -0,0 +1,42 @@
+package cr.ac.tec.Board;
+
+import cr.ac.tec.Board.Manage.GameManager;
+import cr.ac.tec.Board.PathGenerator.*;
+import cr.ac.tec.Board.Square.BlueSquare;
+import cr.ac.tec.Board.Square.*;
+import cr.ac.tec.Images.GetImages;
+import cr.ac.tec.Minigames.PRS.GetGame;
+import cr.ac.tec.Minigames.PRS.PRSGAME;
+import cr.ac.tec.Random.Random;
+import cr.ac.tecLinkedList.List.DoubleList;
+import cr.ac.tecLinkedList.List.DoubleRoundList;
+import cr.ac.tecLinkedList.List.List;
+import cr.ac.tecLinkedList.List.SingleList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage MainWindow) {
+        AnchorPane FirstLevelAnchorPane = LayoutCreation.Anchor(1300, 700);
+        FirstLevelAnchorPane.setStyle("-fx-background-color: #0078d7");
+        GameManager gameManager=GameManager.getInstance(2,5);
+        Button btn=WidgetCreation.CreateButton("Hola");
+        gameManager.Draw(FirstLevelAnchorPane);
+        btn.setOnAction(e->gameManager.StartTurn(2));
+        FirstLevelAnchorPane.getChildren().add(btn);
+        Scene scene =new Scene(FirstLevelAnchorPane,1300,700, Color.RED);
+        MainWindow.setScene(scene);
+        MainWindow.show();
+
+
+    }
+}
