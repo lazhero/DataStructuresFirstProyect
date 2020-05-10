1293bb9e6954fbb4b4066af4413ed9d2e4f35713
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-05-09 18:25:44 -0600

    [DAT-188] RedSquare Event

--- src/cr/ac/tec/Board/Square/RedSquare.java
+++ src/cr/ac/tec/Board/Square/RedSquare.java
@@ -4,6 +4,8 @@ import cr.ac.tec.Board.Player;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
+import java.util.Random;
+
 public class RedSquare extends Square {
     /**
      * Class's contructor
@@ -17,7 +19,19 @@ public class RedSquare extends Square {
 
     @Override
     public void event(Player player) {
-        //lacks event
+
+        System.out.println(player.getCoins());
+        int lose= new Random().nextInt(3)+1;
+        if (player.getCoins()<=lose){
+            player.setCoins(0);
+        }else{
+            int actualCoins= player.getCoins();
+            player.setCoins(actualCoins-lose);
+           
+        }
+
+
+
     }
 
     @Override
