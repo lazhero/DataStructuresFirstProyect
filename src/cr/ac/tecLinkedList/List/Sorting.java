08eb4991e552c4ca2ee4ffbc57ee2c3c823f9b7c
lazhero <54720338+lazhero@users.noreply.github.com>
2020-05-30 06:07:15 -0600

    [DAT-226] Creating the sorting interface

--- src/cr/ac/tecLinkedList/List/Sorting.java
+++ src/cr/ac/tecLinkedList/List/Sorting.java
@@ -1,5 +1,7 @@
 package cr.ac.tecLinkedList.List;
 
 public interface Sorting<T extends Comparable> {
-    public void sort( DoubleList<T> List);
+    public void sort();
+    public void setList(List<T> List);
+
 }
