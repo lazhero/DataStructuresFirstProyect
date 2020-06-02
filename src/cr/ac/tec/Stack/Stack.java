9a12be3da489203ecb7e33fdf69ac1e584da0c5f
lazhero <54720338+lazhero@users.noreply.github.com>
2020-06-02 13:26:32 -0600

    [DAT-237] Adding stack

---
+++ src/cr/ac/tec/Stack/Stack.java
@@ -0,0 +1,28 @@
+package cr.ac.tec.Stack;
+
+import cr.ac.tecLinkedList.List.DoubleList;
+
+public class Stack<T> {
+    private DoubleList<T> reference;
+    public Stack(){
+        reference=new DoubleList<>();
+    }
+    public void push(T content){
+        reference.AddHead(content);
+    }
+    public T pop(){
+        if(!reference.isEmpty()){
+            T temp=reference.get(0);
+            reference.delete(0);
+            return  temp;
+        }
+        return null;
+    }
+    public T view(){
+        if(!reference.isEmpty()){
+            return reference.get(0);
+        }
+        return null;
+    }
+
+}
